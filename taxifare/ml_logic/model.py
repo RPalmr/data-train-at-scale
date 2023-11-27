9a9eeb20afa585df172f9ef204261c4c4b5fde42
Reece Palmer <reecelpalmer@gmail.com>
2023-11-27 16:04:12 +0000

    add project

--- taxifare/ml_logic/model.py
+++ taxifare/ml_logic/model.py
@@ -16,12 +16,22 @@ end = time.perf_counter()
 print(f"\n✅ TensorFlow loaded ({round(end - start, 2)}s)")
 
 
-
 def initialize_model(input_shape: tuple) -> Model:
     """
     Initialize the Neural Network with random weights
     """
-    # YOUR CODE HERE
+
+    reg = regularizers.l1_l2(l1=0.005)
+
+    model = Sequential()
+    model.add(layers.Input(shape=input_shape))
+    model.add(layers.Dense(100, activation="relu", kernel_regularizer=reg))
+    model.add(layers.BatchNormalization(momentum=0.9))
+    model.add(layers.Dropout(rate=0.1))
+    model.add(layers.Dense(50, activation="relu"))
+    model.add(layers.BatchNormalization(momentum=0.9))  # use momentum=0 to only use statistic of the last seen minibatch in inference mode ("short memory"). Use 1 to average statistics of all seen batch during training histories.
+    model.add(layers.Dropout(rate=0.1))
+    model.add(layers.Dense(1, activation="linear"))
 
     print("✅ Model initialized")
 
@@ -32,11 +42,13 @@ def compile_model(model: Model, learning_rate=0.0005) -> Model:
     """
     Compile the Neural Network
     """
-    # YOUR CODE HERE
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(loss="mean_squared_error", optimizer=optimizer, metrics=["mae"])
 
     print("✅ Model compiled")
 
     return model
+
 
 def train_model(
         model: Model,
@@ -44,15 +56,29 @@ def train_model(
         y: np.ndarray,
         batch_size=256,
         patience=2,
-        validation_data=None, # overrides validation_split
+        validation_data=None,  # overrides validation_split
         validation_split=0.3
-    ) -> Tuple[Model, dict]:
+) -> Tuple[Model, dict]:
     """
     Fit the model and return a tuple (fitted_model, history)
     """
-    # YOUR CODE HERE
+    es = EarlyStopping(
+    monitor="val_loss",
+    patience=2,
+    restore_best_weights=True,
+    verbose=0
+)
+
+    history = model.fit(
+        X,
+        y,
+        validation_data=(validation_data),
+        epochs=100,
+        batch_size=batch_size,
+        callbacks=[es],
+        verbose=1
+    )
 
     print(f"✅ Model trained on {len(X)} rows with min val MAE: {round(np.min(history.history['val_mae']), 2)}")
 
     return model, history
-
